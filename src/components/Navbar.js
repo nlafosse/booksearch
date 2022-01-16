d3b1442e2e9a52121c1fa29b82cafc8a5a441957
Nathalia Lafosse <lafossenathalia@gmail.com>
2022-01-15 22:02:43 -0500

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,32 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { BiSearchAlt } from "react-icons/bi";
 
-const Navbar = () => {
+const NavBarTwo = () => {
   return (
-    <div className="navbar">
-      <div>
-        <p className="logo">
-          <Link to="/">BookListed</Link>
-        </p>
+    <div class="w-screen flex flex-row items-center p-1 justify-between bg-white shadow-xs">
+      <div class="ml-8 text-lg text-gray-700 hidden md:flex">Booklisted</div>
+      <span class="w-screen md:w-1/3 h-10 bg-gray-200 cursor-pointer border border-gray-300 text-sm rounded-full flex">
+        <input
+          type="search"
+          name="serch"
+          placeholder="Search"
+          class="flex-grow px-4 rounded-l-full rounded-r-full text-sm focus:outline-none"
+        />
+        <i class="fas fa-search m-3 mr-5 text-lg text-gray-700 w-4 h-4">+</i>
+      </span>
+      <div class="flex flex-row-reverse mr-4 ml-4 md:hidden">
+        <i class="fas fa-bars"></i>
       </div>
-      <div className="navlinks">
-        <Link to="/">Home</Link>
-        <Link to="/subjects">Genres</Link>
-        <Link to="/about">About</Link>
+      <div class="flex flex-row-reverse mr-8 hidden md:flex">
+        <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+          Button
+        </div>
+        <div class="text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
+          Link
+        </div>
       </div>
     </div>
   );
 };
 
-export default Navbar;
+export default NavBarTwo;
