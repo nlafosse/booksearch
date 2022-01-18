4ebf0fe2454696708f3d420d84571156fc542f0b
Nathalia Lafosse <lafossenathalia@gmail.com>
2022-01-18 14:14:13 -0500

    tinkering with css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,9 +13,6 @@ const NavBar = () => {
 
       <Searchbar />
 
-      <div className="flex flex-row-reverse mr-4 ml-4 md:hidden">
-        <i className="fas fa-bars"></i>
-      </div>
       <div className="flex flex-row mr-8 hidden md:flex">
         <Link to={"/authors"}>
           <div className="text-gray-700 text-center px-4 py-2 m-2">Authors</div>
