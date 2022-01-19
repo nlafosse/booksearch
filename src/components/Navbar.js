4f85e7edd325f27bc6d043f6afd21898dca2dc8f
Nathalia Lafosse <lafossenathalia@gmail.com>
2022-01-19 11:08:13 -0500

    tailwind css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import Searchbar from "./Searchbar";
 
 const NavBar = () => {
   return (
-    <div>
+    <div className="container">
       <div>
         <Link to={"/"}>
           <h3 className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded">
