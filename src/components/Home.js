fca4bba7eaf9f2b02867a1031c6f0686dc33904f
Nathalia Lafosse <lafossenathalia@gmail.com>
2022-01-28 13:02:41 -0500

    navbar css

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,14 @@ import React from "react";
 const Home = () => {
   return (
     <div className="container mx-auto bg-grey-100">
-      <div className="flex justify-center ">
-        <h1>HOME</h1>
-        <img
-          alt="gator surrounded by books"
-          src="https://media0.giphy.com/media/9Dk2vkAmYs5dsSRu3B/giphy.gif"
-        />
+      <div className="flex flex-col items-center">
+        <h1 className="p-2">HOME</h1>
+        <div className="p-4">
+          <img
+            alt="gator surrounded by books"
+            src="https://media0.giphy.com/media/9Dk2vkAmYs5dsSRu3B/giphy.gif"
+          />
+        </div>
       </div>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,26 +4,56 @@ import Searchbar from "./Searchbar";
 
 const NavBar = () => {
   return (
-    <div className="container">
-      <div>
+    <nav className="flex items-center justify-between bg-gray-300 text-black font-bold p-2 rounded">
+      <div className="hover:text-white">
         <Link to={"/"}>
-          <h3 className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 rounded">
-            Booklisted
-          </h3>
+          <h3>Booklisted</h3>
         </Link>
       </div>
 
       <Searchbar />
 
-      <div>
-        <Link to={"/authors"}>
-          <div>Authors</div>
-        </Link>
-        <Link to={"/subjects"}>
-          <div>Subjects</div>
-        </Link>
+      <div className="flex flex-row">
+        <div className="hover:text-white">
+          <Link to={"/authors"}>Authors</Link>
+        </div>
+        <div className="hover:text-white">
+          <Link to={"/subjects"}>Subjects</Link>
+        </div>
+        <div>
+          <button
+            type="button"
+            class="block text-gray-500 hover:text-white focus:text-white focus:outline-none"
+          >
+            <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+              <path
+                fill-rule="evenodd"
+                d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
+              />
+            </svg>
+          </button>
+        </div>
       </div>
-    </div>
+    </nav>
+
+    // <div className="flex bg-gray-300 text-black font-bold py-2 px-4 rounded">
+    //   <div className="hover:text-gray-200">
+    // <Link to={"/"}>
+    //   <h3>Booklisted</h3>
+    // </Link>
+    //   </div>
+
+    //   <Searchbar />
+
+    // <div className="flex flex-row justify-between">
+    //   <div className="hover:text-gray-200">
+    //     <Link to={"/authors"}>Authors</Link>
+    //   </div>
+    //   <div className="hover:text-gray-200">
+    //     <Link to={"/subjects"}>Subjects</Link>
+    //   </div>
+    // </div>
+    // </div>
   );
 };
 
