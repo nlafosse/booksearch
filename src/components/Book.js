5c6d35175a89d20a8ba99b4748d783c80a70f3ba
Nathalia Lafosse <lafossenathalia@gmail.com>
2022-01-29 01:12:30 -0500

    descriptions

--- src/components/Book.js
+++ src/components/Book.js
@@ -29,23 +29,20 @@ const Book = (props) => {
 
   return (
     <div className="flex justify-center">
-      <article className="bg-gray-100 py-5 px-10 rounded-lg sm:px-5">
+      <article className="w-1/2 bg-gray-100 py-5 px-10 rounded-lg sm:px-5">
         <img src={images.thumbnail} className="box-border rounded h-64" />
         <h2 className="font-bold">{book.title}</h2>
-        <section>
-          {authorName.map((name) => {
-            return (
-              <div>
-                <p className="mt-2 my-2">
-                  by:{" "}
-                  <Link to={`/authors/${name.split(" ").join("+")}`}>
-                    {name}
-                  </Link>
-                </p>
-              </div>
-            );
-          })}
-        </section>
+        {authorName.map((name) => {
+          return (
+            <div>
+              <p className="mt-2 my-2">
+                by:{" "}
+                <Link to={`/authors/${name.split(" ").join("+")}`}>{name}</Link>
+              </p>
+            </div>
+          );
+        })}
+        <p>{book.description}</p>
       </article>
     </div>
     // {/* {book.imageLinks.map((link) => {
