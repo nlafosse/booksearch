187b265fd2010b70dc29af7f535927bd5dae29cb
Nathalia Lafosse <lafossenathalia@gmail.com>
2022-01-31 21:03:02 -0500

    responsive menu

--- src/components/Menu.js
+++
@@ -1,5 +0,0 @@
-const Menu = () => (
-  <nav className="p-4 flex flex-col space-y-3 md:hidden">{children}</nav>
-);
-
-export default Menu;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ const NavBar = () => {
 
   return (
     <div className="shadow-md w-full top-0 left-0">
-      <div className="md:flex items-center justify-between bg-orange-300 py-4 md:px-10 px-7 font-[Poppins]">
+      <div className="md:flex items-center justify-between bg-orange-300 py-4 md:px-10 px-7 font-[font-mono]">
         <div className="font-bold text-2xl cursor-pointer flex items-center  text-gray-800">
           <Link to={"/"}>
             <h2 className="hover:text-white pr-20">Booklisted</h2>
@@ -29,13 +29,14 @@ const NavBar = () => {
         </div>
 
         <ul
-          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static  bg-orange-300 md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
-            open ? "top-50 opacity-100" : "top-[-490px]"
+          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static  bg-orange-300 md:z-auto left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+            open ? "top-70 opacity-100" : "top-[-490px]"
           } `}
         >
           {links.map((link) => (
             <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
               <Link
+                onClick={() => setOpen(!open)}
                 className="text-gray-800 hover:text-gray-400 duration-500"
                 to={link.link}
               >
