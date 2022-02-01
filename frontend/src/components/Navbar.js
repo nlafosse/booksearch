438d85f047e7ed7b55b36ffd64270b536c99ba5b
Nathalia Lafosse <lafossenathalia@gmail.com>
2022-01-31 22:48:11 -0500

    express mongoose

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -12,10 +12,10 @@ const NavBar = () => {
 
   return (
     <div className="shadow-md w-full top-0 left-0">
-      <div className="md:flex items-center justify-between bg-orange-300 py-4 md:px-10 px-7 font-[font-mono]">
-        <div className="font-bold text-2xl cursor-pointer flex items-center  text-gray-800">
+      <div className="md:flex items-center justify-between bg-orange-300 py-4 md:px-10 px-7 mono-['Courier New']">
+        <div className="font-bold text-2xl cursor-pointer flex items-center  text-black">
           <Link to={"/"}>
-            <h2 className="hover:text-white pr-20">Booklisted</h2>
+            <h2 className="hover:text-white duration-500 pr-20">Booklisted</h2>
           </Link>
         </div>
 
@@ -29,7 +29,7 @@ const NavBar = () => {
         </div>
 
         <ul
-          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static  bg-orange-300 md:z-auto left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-orange-300 md:z-auto left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
             open ? "top-70 opacity-100" : "top-[-490px]"
           } `}
         >
@@ -37,7 +37,7 @@ const NavBar = () => {
             <li key={link.name} className="md:ml-8 text-xl md:my-0 my-7">
               <Link
                 onClick={() => setOpen(!open)}
-                className="text-gray-800 hover:text-gray-400 duration-500"
+                className="text-gray-800 hover:text-white duration-500"
                 to={link.link}
               >
                 {link.name}
